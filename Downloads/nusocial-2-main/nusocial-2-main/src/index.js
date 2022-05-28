e9de2319e1a9163b90634249fad7244ce545572b
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-05-28 20:22:43 +0530

    updates

---
+++ Downloads/nusocial-2-main/nusocial-2-main/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home/Home';
+import RegisterStudent from './pages/Register/register-student';
+import LoginStudent from "./pages/Login/login-student"
+import PersonalChat from "./pages/PersonalChat/PersonalChat"
+import { LaunchPage } from './pages/LaunchPage/LaunchPage';
+import QuickLinks from "./pages/QuickLinks/QuickLinks"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <Routes>
+  <Route path = "/" element = {<LaunchPage />} />
+  <Route path = "/home" element = { <Home />} />
+  <Route path = "/students-register" element = { <RegisterStudent />} />
+  <Route path = "/students-login" element = {<LoginStudent/>} />
+  <Route path = "/personalchat" element = {<PersonalChat />} />
+  <Route path = "/quicklinks" element = {<QuickLinks />} />
+  </Routes> 
+  </BrowserRouter>
+);
